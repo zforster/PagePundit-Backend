a114add1a9e9c61be1af855e0810f7ee3a11cf82
Zak Forster <zak.forster16@gmail.com>
2023-04-03 22:39:44 +0100

    fixes

--- src/lambdas/recommendations/service.py
+++ src/lambdas/recommendations/service.py
@@ -1,12 +1,10 @@
 import json
-
-from models.book import Book
+import logging
 
 from common.threads.threads import call_with_threads
+from lambdas.recommendations.models.book import Book
 from lambdas.recommendations.wrappers.google_books_wrapper import GoogleBooksWrapper
-from lambdas.recommendations.wrappers.open_ai_wrapper import (
-    AbstractOpenAIWrapper,
-)
+from lambdas.recommendations.wrappers.open_ai_wrapper import AbstractOpenAIWrapper
 
 
 def get_recommendations_from_text(
@@ -15,7 +13,7 @@ def get_recommendations_from_text(
     user_input: str,
 ) -> str:
     open_ai_response = open_ai_wrapper.query(
-        temperature=0.5,
+        temperature=0.4,
         messages=[
             {
                 "role": "system",
@@ -35,12 +33,16 @@ def get_recommendations_from_text(
             },
         ],
     )
-    open_ai_response_as_dict: list[dict] = json.loads(open_ai_response)
-    google_books_responses = call_with_threads(
-        function=google_books_wrapper.request_book,
-        function_input=open_ai_response_as_dict,
-    )
-    google_books_responses: list[Book] = [
-        response for response in google_books_responses if response
-    ]
-    return json.dumps([book.to_dict_by_alias() for book in google_books_responses])
+    try:
+        open_ai_response_as_dict: list[dict] = json.loads(open_ai_response)
+        google_books_responses = call_with_threads(
+            function=google_books_wrapper.request_book,
+            function_input=open_ai_response_as_dict,
+        )
+        google_books_responses: list[Book] = [
+            response for response in google_books_responses if response
+        ]
+        return json.dumps([book.to_dict_by_alias() for book in google_books_responses])
+    except json.JSONDecodeError:
+        logging.error(f"open ai response - {open_ai_response}")
+        return "[]"
