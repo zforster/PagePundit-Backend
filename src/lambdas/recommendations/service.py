21161de4e88c6f2f8ee8a269818119b5389e68a5
Zak Forster <zak.forster16@gmail.com>
2023-04-05 23:56:42 +0100

    return user input

--- src/lambdas/recommendations/service.py
+++ src/lambdas/recommendations/service.py
@@ -2,7 +2,7 @@ import json
 import logging
 
 from common.threads.threads import call_with_threads
-from lambdas.recommendations.models.book import Book
+from lambdas.recommendations.models.book import Book, BookRecommendationResponse
 from lambdas.recommendations.wrappers.google_books_wrapper import GoogleBooksWrapper
 from lambdas.recommendations.wrappers.open_ai_wrapper import AbstractOpenAIWrapper
 
@@ -39,10 +39,17 @@ def get_recommendations_from_text(
             function=google_books_wrapper.request_book,
             function_input=open_ai_response_as_dict,
         )
-        google_books_responses: list[Book] = [
-            response for response in google_books_responses if response
-        ]
-        return json.dumps([book.to_dict_by_alias() for book in google_books_responses])
+        return json.dumps(
+            BookRecommendationResponse(
+                books=[response for response in google_books_responses if response],
+                user_input=user_input,
+            ).to_dict_by_alias()
+        )
     except json.JSONDecodeError:
         logging.error(f"open ai response - {open_ai_response}")
-        return "[]"
+        return json.dumps(
+            BookRecommendationResponse(
+                books=[],
+                user_input=user_input,
+            ).to_dict_by_alias()
+        )
