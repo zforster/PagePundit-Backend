7943346b4593514726efe1f77b3d5f02d14fa7d1
Zak Forster <zak.forster16@gmail.com>
2024-07-19 21:21:58 +0100

    fix

--- src/common/wrappers/open_ai_wrapper.py
+++ src/common/wrappers/open_ai_wrapper.py
@@ -5,8 +5,7 @@ import openai
 
 
 class Models(str, Enum):
-    GTP_TURBO = "gpt-3.5-turbo"
-    DAVINCI = "text-davinci-003"
+    GPT_4O_MINI = "gpt-4o-mini"
 
 
 class Message(TypedDict):
@@ -18,17 +17,6 @@ class OpenAIWrapper:
     def __init__(self, api_key: str):
         self.api_key = api_key
 
-    def query(self, user_input: str, temperature: Optional[float] = 0.7) -> str:
-        openai.api_key = self.api_key
-        completions = openai.Completion.create(
-            model=Models.DAVINCI,
-            temperature=temperature,
-            prompt=user_input,
-            max_tokens=1000,
-            n=1,
-        )
-        return completions["choices"][0]["text"]
-
     def chat(
         self,
         messages: list[Message],
@@ -36,6 +24,6 @@ class OpenAIWrapper:
     ) -> str:
         openai.api_key = self.api_key
         completions = openai.ChatCompletion.create(
-            model=Models.GTP_TURBO, temperature=temperature, messages=messages
+            model=Models.GPT_4O_MINI, temperature=temperature, messages=messages
         )
         return completions.choices[0]["message"]["content"]
